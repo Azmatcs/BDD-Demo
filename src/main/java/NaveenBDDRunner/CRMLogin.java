f0dbd03ddeb1f65ca24fb4f741b1252b818e2c5a
Azmatcs <77793308+Azmatcs@users.noreply.github.com>
2024-08-01 20:44:31 +0530

    Update CRMLogin.java

--- src/main/java/NaveenBDDRunner/CRMLogin.java
+++ src/main/java/NaveenBDDRunner/CRMLogin.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features="src/main/java/Features", glue="NaveenBDDStep",
+		features="src/main/java/Features/NaveenBDDCRM.feature", glue="NaveenBDDStep",
 		
 		monochrome=true,plugin= {"html:target/NaveenBDDStep.html","pretty"})
 public class CRMLogin extends AbstractTestNGCucumberTests{
